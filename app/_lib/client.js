95d3bf38ebe6752b807dea7b2ea732afd774d964
Mizuki <mbaba@kanolab.net>
2023-11-25 14:54:07 +0900

    [fix] add custom fetch

--- app/_lib/client.js
+++ app/_lib/client.js
@@ -3,4 +3,14 @@ import { createClient } from "microcms-js-sdk";
 export const client = createClient({
   serviceDomain: "mokumoku-blog",
   apiKey: process.env.API_KEY,
+  retry: true,
+  customFetch: (input, init) => {
+    if (typeof input === "string") {
+      const newInput = new URL(input);
+      const time = new Date();
+      newInput.searchParams.set("cacheclearparam", `${time.getMinutes()}`);
+      return fetch(newInput.href, init);
+    }
+    return fetch(input, init);
+  },
 });
